ca4847ef2119acc1678b5728298b2c327676d0d4
Ranju Raveendran <ranju.raveendran@yoobeecolleges.com>
2022-12-08 16:16:25 +1300

    initial commit

---
+++ portfolio-website-ranju/src/App.js
@@ -0,0 +1,24 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Ranju Raveendran</h1>
+        <h3> Fullstack Developer</h3>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/ranju-raveendran-3588b91a/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          LinkedIn
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
