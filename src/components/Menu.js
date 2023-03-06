c750d9d908185a2daa7f1373a510c5360573f1ab
Ranju Raveendran <ranju.raveendran@yoobeecolleges.com>
2023-03-07 09:04:16 +1300

    a link tag fixed

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -4,12 +4,12 @@ function Menu() {
   return (
     <div className='NavMenu'>
         <ul id="menu">
-          <a href="#"><li>Home</li></a>
-          <a href="#"><li>About</li></a>
-          <a href="#"><li>Experience</li></a>
-          <a href="#"><li>Works</li></a>
-          <a href="#"><li>Education</li></a>
-          <a href="#"><li>Contact</li></a>
+          <div id='menu_item'><li>Home</li></div>
+          <div id='menu_item'><li>About</li></div>
+          <div id='menu_item'><li>Experience</li></div>
+          <div id='menu_item'><li>Works</li></div>
+          <div id='menu_item'><li>Education</li></div>
+          <div id='menu_item'><li>Contact</li></div>
         </ul>
     </div>
   )
