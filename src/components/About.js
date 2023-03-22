3bb35a9dfcb30db1037521727ed0d80c07b7962a
Ranju Raveendran <ranju.raveendran@yoobeecolleges.com>
2023-03-22 22:02:37 +1300

    footer added, download file working

--- src/components/About.js
+++ src/components/About.js
@@ -11,7 +11,10 @@ function About() {
           <p className= {styles.hello}>hello I am</p>
           <p className={styles.name}>Ranju Raveendran</p>
           <p className={styles.developer}>Web and Game Developer</p>
-          <button className={styles.downloadbutton}>Download CV</button>
+          <p className={styles.blob}>Experience in software development and 3D animation. Proficient in Web, Game, XR Development and 3D Graphics. Completed Master of Creative Technologies, Degree in Computer Science and Advanced Software Development courses.</p>
+          <a href="/files/RanjuRaveendranCV.pdf" download>
+            <button className={styles.downloadbutton}>Download CV</button>
+          </a>
         </div>
         <div className={styles.socials}>
           <a className={styles.socialslink} href="https://www.linkedin.com/in/ranjurave/" target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a>
