6c66a7dc5989d25b4bcb4bc80510f08466d11b4b
Ranju Raveendran <39932642+ranjurave@users.noreply.github.com>
2023-03-24 11:02:36 +1300

    bug fix

--- src/components/FloatingIsland.js
+++ src/components/FloatingIsland.js
@@ -8,11 +8,11 @@ export function FloatingIsland() {
     useEffect(() =>{
         if(!gltf) return;
         let mesh = gltf.scene.children[0];
-        var uvs = mesh.geometry.attributes.uv.array;
-        mesh.geometry.setAttribute( 'uv2', new BufferAttribute( uvs, 2 ) );
-        mesh.material.lightMap = mesh.material.map;
-        mesh.material.lightMapIntensity = 400;
-        mesh.material.color = new Color(0.04, 0.06, 0.1);   
+        //var uvs = mesh.geometry.attributes.uv.array;
+        //mesh.geometry.setAttribute( 'uv2', new BufferAttribute( uvs, 2 ) );
+        //mesh.material.lightMap = mesh.material.map;
+        //mesh.material.lightMapIntensity = 400;
+        //mesh.material.color = new Color(0.04, 0.06, 0.1);   
     }, [gltf]);
     return (
         <primitive object={gltf.scene} />
